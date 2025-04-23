3d14b507a979d3095d9c9058da00733aca01e185
cooleo273 <leulteferi273@gmail.com>
2025-04-23 19:31:59 +0300

    refactor: Remove unused authentication and user management routes

--- app/api/submission_photos/[id]/routes.ts
+++
@@ -1,9 +0,0 @@
-import SubmissionController from "@/lib/controllers/submissions.controller";
-import { NextRequest } from "next/server";
-
-const submissionController = new SubmissionController();
-
-export async function GET(req: NextRequest, {params}: {params: {id: string}}) {
-const {id} = await params// Extract the ID from the request parameters
-return submissionController.getSubmissionPhotosById(req, id); // Pass the ID to the controller method
-}
--- app/api/submissions/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import SubmissionController from "../../../lib/controllers/submissions.controller";
-
-const submissionController = new SubmissionController();
-
-export async function GET(req: NextRequest) {
-    return submissionController.getSubmissions(req);
-}
